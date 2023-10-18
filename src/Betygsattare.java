c06345a0359301ae894447d4c8deafdd0d53f578
albsva <albin.svanberg@elev.ga.ntig.se>
2023-10-18 09:23:26 +0200

    Jag har skrivit koden

---
+++ src/Betygsattare.java
@@ -0,0 +1,44 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+public class Betygsattare {
+    public static void main(String[] args)  {
+
+        Scanner tgb = new Scanner(System.in);
+
+        System.out.println("Skriv in dina poäng här:");
+        int score = tgb.nextInt();
+        tgb.nextLine();
+
+        if
+                  ((score < 14) && (score < 18)){
+                  System.out.print("Betyget blev E");}
+
+
+
+        if
+                ((score < 17) && (score < 21)){
+                System.out.print("Betyget blev E");}
+
+
+
+        if
+                ((score < 20) && (score < 24)){
+                System.out.print("Betyget blev E");}
+
+
+
+        if
+                 ((score < 23) && (score < 17)){
+                 System.out.print("Betyget blev E");}
+
+
+
+        if
+                 ((score < 14) && (score < 18)){
+                 System.out.print("Betyget blev E");}
+
+
+
+        }
+        }
